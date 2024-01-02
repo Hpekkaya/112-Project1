ef8fe301783ec54fbe909cb3c2c8b937c1ef87d3
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-02 13:35:14 +0300

    const's assigned

---
+++ trial.js
@@ -0,0 +1,18 @@
+const formWrapper = document.querySelector(".form-wrapper")
+const form = document.querySelector("#form")
+const searchInput = document.querySelector("#searhInput")
+const buttonWrapper = document.querySelector(".button-wrapper")
+const searchButton = document.querySelector("#searchButton")
+const clearButton = document.querySelector("#clearButton")
+const imageListWrapper = document.querySelector(".imagelist-wrapper")
+
+// runs Events 
+runEvetListeners();
+function runEvetListeners(){
+    form.addEventListener("submit", search)
+}
+
+
+function search(e){
+    consol
+}
